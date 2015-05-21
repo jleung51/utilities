8de902f83e7e50b36a48bfe269103d3d599723a7
Jeffrey Leung <j.leung51@shaw.ca>
2015-05-21 12:39:06 -0700

    Updating from #ifndef to #pragma once

--- return_change/change.hpp
+++ return_change/change.hpp
@@ -1,7 +1,5 @@
-#ifndef CHANGE_HPP
-#define CHANGE_HPP
+#pragma once
 
 // This function displays the change necessary, if any, for a given purchase.
 void return_change( double price, double money_given );
 
-#endif
