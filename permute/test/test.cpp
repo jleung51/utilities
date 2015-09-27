7e55e31fdc458deee81e83e6c6765c0463674b11
Jeffrey Leung <jleung513@gmail.com>
2015-09-27 15:02:42 -0700

    Permute: Adding testing for subsequence permutations.

--- permute/test/test.cpp
+++ permute/test/test.cpp
@@ -16,18 +16,32 @@
 
 int main()
 {
-  std::string possibilities = "aabc";
-  std::vector<std::string>* permutations = Permute( possibilities );
+  std::string possibilities1 = "aabc";
+  std::vector<std::string>* permutations1 = Permute( possibilities1, false );
 
   std::cout << "Permutations of the characters 'aabc':"
             << std::endl;
             
-  for( unsigned int i = 0; i < permutations->size(); ++i )
+  for( unsigned int i = 0; i < permutations1->size(); ++i )
   {
-    std::cout << permutations->at(i)
+    std::cout << permutations1->at(i)
               << std::endl;
   }
+  std::cout << std::endl;
+  delete permutations1;
   
-  delete permutations;
+  std::string possibilities2 = "abc";
+  std::vector<std::string>* permutations2 = Permute( possibilities2, true );
+  std::cout << "Subsequence permutations of the characters 'abc':"
+            << std::endl;
+            
+  for( unsigned int i = 0; i < permutations2->size(); ++i )
+  {
+    std::cout << permutations2->at(i)
+              << std::endl;
+  }
+  std::cout << std::endl;
+  delete permutations2;
+  
   return 0;
 }
