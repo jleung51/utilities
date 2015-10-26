811709dd0fe844f15582ea6b8eecd0e2d88d56aa
jleung51 <jleung513@gmail.com>
2015-10-25 18:39:42 -0700

    ReturnChange: Updating formatting and naming.

--- return-change/src/change.cpp
+++ return-change/src/change.cpp
@@ -32,59 +32,59 @@ static void PrintDenomination( unsigned int amount, double denomination )
 
   if( denomination == 100 )
   {
-    std::cout << " $100 bill" ;
+    std::cout << " $100 bill";
   }
   else if( denomination == 50 )
   {
-    std::cout << " $50 bill" ;
+    std::cout << " $50 bill";
   }
   else if( denomination == 20 )
   {
-    std::cout << " $20 bill" ;
+    std::cout << " $20 bill";
   }
   else if( denomination == 10 )
   {
-    std::cout << " $10 bill" ;
+    std::cout << " $10 bill";
   }
   else if( denomination == 5 )
   {
-    std::cout << " $5 bill" ;
+    std::cout << " $5 bill";
   }
   else if( denomination == 2 )
   {
-    std::cout << " toonie" ;
+    std::cout << " toonie";
   }
   else if( denomination == 1 )
   {
-    std::cout << " loonie" ;
+    std::cout << " loonie";
   }
   else if( denomination == 0.25 )
   {
-    std::cout << " quarter" ;
+    std::cout << " quarter";
   }
   else if( denomination == 0.10 )
   {
-    std::cout << " dime" ;
+    std::cout << " dime";
   }
   else if( denomination == 0.05 )
   {
-    std::cout << " nickel" ;
+    std::cout << " nickel";
   }
   else if( denomination == 0.01 )
   {
     if( amount == 1 )
     {
-      std::cout << " penny" ;
+      std::cout << " penny";
     }
     else
     {
-      std::cout << " pennies" ;
+      std::cout << " pennies";
     }
   }
   else
   {
     std::cout << "Error: PrintDenomination() was given an invalid denomination (" << denomination\
-                  << ").\nBugfix required.\n" ;
+                  << ").\nBugfix required.\n";
     exit( 2 );
   }
 
@@ -104,12 +104,12 @@ void ReturnChange( double price, double money_given )
 {
   if( price <= 0 )
   {
-    std::cout << "Error: That is not a valid price.\n" ;
+    std::cout << "Error: That is not a valid price.\n";
     exit( 1 );
   }
   else if( money_given < 0 )
   {
-    std::cout << "That is not a valid amount for money given.\n" ;
+    std::cout << "That is not a valid amount for money given.\n";
     exit( 1 );
   }
 
@@ -117,21 +117,21 @@ void ReturnChange( double price, double money_given )
 
   if( change == 0 )
   {
-    std::cout << "Exact change.\n" ;
+    std::cout << "Exact change.\n";
   }
   else if( change < 0 )
   {
     change = 0 - change;
 
-    std::cout << "Not enough money was given; $" ;
-    std::cout << std::setprecision(2) << std::fixed << change ;  // Prints only two decimal places
-    std::cout << " more is required.\n" ;
+    std::cout << "Not enough money was given; $";
+    std::cout << std::setprecision(2) << std::fixed << change;  // Prints only two decimal places
+    std::cout << " more is required.\n";
   }
   else
   {
-    std::cout << "The change is $" ;
-    std::cout << std::setprecision(2) << std::fixed << change ;  // Prints only two decimal places
-    std::cout << ", which is:\n" ;
+    std::cout << "The change is $";
+    std::cout << std::setprecision(2) << std::fixed << change;  // Prints only two decimal places
+    std::cout << ", which is:\n";
 
     double denominations[11] = { 100, 50, 20, 10, 5, 2, 1, 0.25, 0.10, 0.05, 0.01 };
     unsigned int leftover;
