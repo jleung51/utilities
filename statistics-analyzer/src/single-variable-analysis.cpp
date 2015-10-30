28f4f126766ee2d9dad27d191c606121b9a1941a
jleung51 <jleung513@gmail.com>
2015-10-29 23:40:24 -0700

    StatAnalysis: Adding includes and unnamed namespace.

--- statistics-analyzer/src/single-variable-analysis.cpp
+++ statistics-analyzer/src/single-variable-analysis.cpp
@@ -3,6 +3,23 @@
  * Author: Jeffrey Leung
  * Last edited: 2015-10-29
  *
- * This C++ file contains functions which analyze a single dataset.
+ * This C++ file contains functions which analyze a single dataset,
+ * a vector of values.
  *
  */
+
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+#include "../include/single-variable-analysis.hpp"
+
+namespace  // Only available within this file
+{
+
+// This private function returns true if a given dataset is sorted in
+// ascending order, and false otherwise.
+template <class T>
+bool IsSorted( std::vector<T> dataset );
+
+}  // Unnamed namespace
