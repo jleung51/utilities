b27e65cdef49e30c970f021910c4d7d52b9be012
jleung51 <jleung513@gmail.com>
2015-10-30 14:42:20 -0700

    StatAnalysis: Adding implementation of IsSorted().

--- statistics-analyzer/src/single-variable-analysis.cpp
+++ statistics-analyzer/src/single-variable-analysis.cpp
@@ -17,9 +17,28 @@
 namespace  // Only available within this file
 {
 
-// This private function returns true if a given dataset is sorted in
+// Function prototypes:
+
+// This private function returns true if a given vector dataset is sorted in
 // ascending order, and false otherwise.
 template <class T>
 bool IsSorted( std::vector<T> dataset );
+
+// Function implementations:
+
+// This private function returns true if a given vector dataset is sorted in
+// ascending order, and false otherwise.
+template <class T>
+bool IsSorted( std::vector<T> dataset )
+{
+  for( unsigned int i = 0; i < dataset.size()-1; ++i )
+  {
+    if( dataset[i] > dataset[i+1] )
+    {
+      return false;
+    }
+  }
+  return true;
+}
 
 }  // Unnamed namespace
