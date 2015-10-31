c98d9b8612032d67a5300f2d6e6de8d5082a15ab
jleung51 <jleung513@gmail.com>
2015-10-30 23:06:59 -0700

    Permute: Updating part of merge sort to vectors; adding sort testing.

--- permute/include/merge_sort_vector.hpp
+++ permute/include/merge_sort_vector.hpp
@@ -1,13 +1,10 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-10-17
+ * Last edited: 2015-10-30
  *
  * This C++ program contains the implementation of a recursive merge sort
- * algorithm on an array of integers.
- *
- * Originally written for the repository:
- *   https://github.com/jleung51/mini_programs/ - Permute
+ * algorithm on a vector.
  *
  */
 
@@ -15,45 +12,123 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+
+//TODO TESTING ONLY; REMOVE ONCE IMMEDIATE TESTING IS COMPLETE
+// This function prints the contents of a given vector.
+template <class T>
+void PrintVector( std::vector<T> v )
+{
+  std::cout << "[ ";
+  
+  unsigned int i = 0;
+  typename std::vector<T>::iterator v_index;
+  for( v_index = v.begin(); v_index < v.end(); ++v_index, ++i )
+  {
+    std::cout << v.at( i ) << " ";
+  }
+  
+  std::cout << "]";
+
+  return;
+}
+
 
 // Function prototypes:
 
-// This function recursively sorts an array from least to greatest using the
+// This function recursively sorts a vector from least to greatest using the
 // merge sort algorithm.
-// An exception is thrown if:
-//   array is null (invalid_argument)
 template <class T>
-void MergeSort( T* array, unsigned int len );
+void MergeSortVector( std::vector<T>& v );
 
 
 // Static functions:
 namespace
 {
 
-// Only for use by MergeSortHelper().
-// This function merges two sorted arrays to create a single array (in the
-// memory spaces of the original arrays) with elements from least to greatest.
-// array1 and array2 must be in consecutive memory spaces - i.e. two sections
-// of the same array.
+// Only for use by MergeSortVectorHelper().
+// This function merges two sorted vectors (arranged as consecutive elements
+// in a single, larger vector) to create a "single" vector with elements from
+// least to greatest.
 template <class T>
-void MergeArrays( T* array1, unsigned int len1,
-                   T* array2, unsigned int len2 );
-                   
-// Only for use by MergeSort().
-// This function recursively sorts an array from least to greatest using the
-// merge sort algorithm.
-template <class T>
-void MergeSortHelper( T* array, unsigned int len );
+void MergeVectors( std::vector<T>& v,
+                   typename std::vector<T>::iterator v1_start,
+                   typename std::vector<T>::iterator v1_end,
+                   typename std::vector<T>::iterator v2_end );
 
-// Only for use by MergeSortHelper().
-// This function merges two sorted arrays to create a single array (in the
-// memory spaces of the original arrays) with elements from least to greatest.
-// array1 and array2 must be in consecutive memory spaces - i.e. two sections
-// of the same array.
+// Only for use by MergeSortVectorHelper().
+// This function merges two sorted vectors (arranged as consecutive elements
+// in a single, larger vector) to create a "single" vector with elements from
+// least to greatest.
 template <class T>
-void MergeArrays( T* array1, unsigned int len1,
-                   T* array2, unsigned int len2 )
+void MergeVectors( std::vector<T>& v,
+                   typename std::vector<T>::iterator v1_start,
+                   typename std::vector<T>::iterator v1_end,
+                   typename std::vector<T>::iterator v2_end )
 {
+  /* // Code to merge two vectors (non-functional)
+  std::vector<T> v_temp;
+  
+  typename std::vector<T>::iterator v1_index = v1.begin();
+  typename std::vector<T>::iterator v2_index = v2.begin();
+  
+  unsigned int i_1 = 0;
+  unsigned int i_2 = 0;
+  
+  while( v1_index < v1.end() &&
+         v2_index < v2.end() )
+  {
+    if( v1[i_1] < v2[i_2] )
+    {
+      v_temp.insert( v_temp.end(), v1[i_1] );
+      ++v1_index;
+      ++i_1;
+    }
+    else
+    {
+      v_temp.insert( v_temp.end(), v2[i_2] );
+      ++v2_index;
+      ++i_2;
+    }
+  }
+  
+  while( i_1 < v1.size() )
+  {
+    v_temp.insert( v_temp.end(), v1[i_1] );
+    ++i_1;
+  }
+  while( i_2 < v2.size() )
+  {
+    v_temp.insert( v_temp.end(), v2[i_2] );
+    ++i_2;
+  }
+  
+  std::cout << "Sorted array ";
+  PrintVector( v_temp );
+  std::cout << " was created from the arrays ";
+  PrintVector( v1 );
+  PrintVector( v2 );
+  std::cout << std::endl;
+  
+  typename std::vector<T>::iterator v_index;
+  
+  for( unsigned int j = 0; j < v1.size(); ++j )
+  {
+    v1[j] = v_temp[j];
+  }
+  
+  for( unsigned int k = 0; k < v2.size(); ++k )
+  {
+    v2[k] = v_temp[ v1.size() + k ];
+  }
+  
+  std::cout << "Final array: ";
+  PrintVector( v1 );
+  PrintVector( v2 );
+  std::cout << std::endl << std::endl;
+*/
+
+/* // Code to merge templated arrays
   T* array_final = array1;
   unsigned int len_final = len1 + len2;
   T array_copy[len_final];
@@ -92,45 +167,43 @@ void MergeArrays( T* array1, unsigned int len1,
   {
     array_final[j] = array_copy[j];
   }
-
+*/
   return;
 }
 
-// Only for use by MergeSort().
-// This function recursively sorts an array from least to greatest using the
+// Only for use by MergeSortVector().
+// This function recursively sorts a vector from least to greatest using the
 // merge sort algorithm.
 template <class T>
-void MergeSortHelper( T* array, unsigned int len )
+void MergeSortVectorHelper( std::vector<T>& v,
+                            typename std::vector<T>::iterator start,
+                            typename std::vector<T>::iterator end )
 {
-  if( len < 2 )
+  unsigned int size = std::distance(start, end);
+  if( size < 2 )
   {
     return;
   }
-  unsigned int mid = len / 2;
-  MergeSortHelper( array,     mid );
-  MergeSortHelper( array+mid, len-mid );
-
-  MergeArrays( array, mid, array+mid, len-mid );
+  
+  unsigned int mid = size / 2;
+  
+  MergeSortVectorHelper( v, start, start+mid );
+  MergeSortVectorHelper( v, start+mid, end );
+  
+  //MergeVectors( v1, v2 );
   
   return;
 }
 
-}  // Namespace for static functions
+}  // Namespace for private functions
+
 
 // Functions:
 
-// This function recursively sorts an array from least to greatest using the
+// This function recursively sorts a vector from least to greatest using the
 // merge sort algorithm.
-// An exception is thrown if:
-//   array is null (invalid_argument)
 template <class T>
-void MergeSort( T* array, unsigned int len )
+void MergeSortVector( std::vector<T>& v )
 {
-  if( array == nullptr )
-  {
-    throw std::invalid_argument( "Error: MergeSort() was given a null array." );
-  }
-  
-  MergeSortHelper( array, len );
-  return;
+  MergeSortVectorHelper( v, v.begin(), v.end() );
 }
--- permute/test/test_merge_sort.cpp
+++ permute/test/test_merge_sort.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-10-25
+ * Last edited: 2015-10-30
  *
  * This C++ file tests the implementation of the merge sort (vector) function
  * in merge_sort_vector.hpp.
@@ -13,8 +13,42 @@
 
 #include "../include/merge_sort_vector.hpp"
 
+/*
+// This function prints the contents of a given vector.
+template <class T>
+void PrintVector( std::vector<T> v )
+{
+  std::cout << "[ ";
+  
+  unsigned int i = 0;
+  typename std::vector<T>::iterator v_index;
+  for( v_index = v.begin(); v_index < v.end(); ++v_index, ++i )
+  {
+    std::cout << v.at( i ) << " ";
+  }
+  
+  std::cout << "]";
+
+  return;
+}*/
+
 int main()
 {
+  std::vector<int> v;
+  for( unsigned int i = 0; i < 10; ++i )
+  {
+    v.push_back( 10-i );
+  }
+  
+  std::cout << "The contents of the vector before sorting are ";
+  PrintVector( v );
+  std::cout << std::endl;
+  
+  MergeSortVector( v );
+  
+  std::cout << "The contents of the vector after sorting are  ";
+  PrintVector( v );
+  std::cout << std::endl;
 
   return 0;
 }
