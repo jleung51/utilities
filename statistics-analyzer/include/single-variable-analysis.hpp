2b24911b380439a9a90130c1eb0b0aa26bd59b85
jleung51 <jleung513@gmail.com>
2015-10-31 00:07:11 -0700

    StatAnalyzer: Changing functions to take a referenced dataset.

--- statistics-analyzer/include/single-variable-analysis.hpp
+++ statistics-analyzer/include/single-variable-analysis.hpp
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-10-29
+ * Last edited: 2015-10-30
  *
  * This C++ header file contains functions which analyze a single dataset,
  * a vector of values.
@@ -17,39 +17,39 @@
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 template <class T>
-void Sort( std::vector<T> dataset );
+void Sort( std::vector<T>& dataset );
 
 // This function returns the mean of a vector dataset.
 template <class T>
-T Mean( std::vector<T> dataset );
+T Mean( std::vector<T>& dataset );
 
 // This function returns the mode of a vector dataset.
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-T Mode( std::vector<T> dataset );
+T Mode( std::vector<T>& dataset );
 
 // This function returns the median value of a vector dataset.
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-T Median( std::vector<T> dataset );
+T Median( std::vector<T>& dataset );
 
 // This function returns the minimum value of a vector dataset.
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-T Minimum( std::vector<T> dataset );
+T Minimum( std::vector<T>& dataset );
 
 // This function returns the maximum value of a vector dataset.
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-T Maximum( std::vector<T> dataset );
+T Maximum( std::vector<T>& dataset );
 
 // This function returns a given quartile of a vector dataset.
 // An exception is thrown if:
@@ -57,7 +57,7 @@ T Maximum( std::vector<T> dataset );
 //   The dataset is not sorted in ascending order (invalid_argument)
 //   The quartile is not 1, 2, 3, or 4 (invalid_argument).
 template <class T>
-T Quartile( std::vector<T> dataset, unsigned int quartile_num );
+T Quartile( std::vector<T>& dataset, unsigned int quartile_num );
 
 // This function returns the lower limit of a vector dataset.
 // The 1.5 IQR rule is used.
@@ -65,7 +65,7 @@ T Quartile( std::vector<T> dataset, unsigned int quartile_num );
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-unsigned int LowerLimit( std::vector<T> dataset );
+unsigned int LowerLimit( std::vector<T>& dataset );
 
 // This function returns the upper limit of a vector dataset.
 // The 1.5 IQR rule is used.
@@ -73,7 +73,7 @@ unsigned int LowerLimit( std::vector<T> dataset );
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-unsigned int UpperLimit( std::vector<T> dataset );
+unsigned int UpperLimit( std::vector<T>& dataset );
 
 // This function returns the IQR of a vector dataset.
 // The 1.5 IQR rule is used.
@@ -81,7 +81,7 @@ unsigned int UpperLimit( std::vector<T> dataset );
 //   The dataset is empty (length_error)
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
-T InterQuartileRange( std::vector<T> dataset );
+T InterQuartileRange( std::vector<T>& dataset );
 
 // This function returns the outliers of a vector dataset into a given vector.
 // The 1.5 IQR rule is used.
@@ -90,10 +90,10 @@ T InterQuartileRange( std::vector<T> dataset );
 //   The dataset is not sorted in ascending order (invalid_argument)
 //   The outliers vector is not empty (invalid_argument)
 template <class T>
-void Outliers( std::vector<T> dataset, std::vector<T>& outliers );
+void Outliers( std::vector<T>& dataset, std::vector<T>& outliers );
 
 // This function returns the standard deviation of a vector dataset.
 // An exception is thrown if:
 //   The dataset is empty (length_error)
 template <class T>
-T StandardDeviation( std::vector<T> dataset );
+T StandardDeviation( std::vector<T>& dataset );
--- statistics-analyzer/src/single-variable-analysis.cpp
+++ statistics-analyzer/src/single-variable-analysis.cpp
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-10-29
+ * Last edited: 2015-10-30
  *
  * This C++ file contains functions which analyze a single dataset,
  * a vector of values.
@@ -22,14 +22,14 @@ namespace  // Only available within this file
 // This private function returns true if a given vector dataset is sorted in
 // ascending order, and false otherwise.
 template <class T>
-bool IsSorted( std::vector<T> dataset );
+bool IsSorted( std::vector<T>& dataset );
 
 // Function implementations:
 
 // This private function returns true if a given vector dataset is sorted in
 // ascending order, and false otherwise.
 template <class T>
-bool IsSorted( std::vector<T> dataset )
+bool IsSorted( std::vector<T>& dataset )
 {
   for( unsigned int i = 0; i < dataset.size()-1; ++i )
   {
