5cb771874f9232dbdbe5d91d25756d318f289aaa
jleung51 <jleung513@gmail.com>
2015-10-31 01:02:00 -0700

    StatAnalyzer: Implementing testing for Sort().

--- statistics-analyzer/test/test.cpp
+++ statistics-analyzer/test/test.cpp
@@ -7,7 +7,9 @@
  *
  */
 
+#include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #include "print-data-structures.hpp"
 #include "../include/merge-sort-vector.hpp"
@@ -17,6 +19,32 @@
 
 int main()
 {
+  std::cout << std::endl
+            << "TESTING SINGLE VARIABLE ANALYSIS:"
+            << std::endl
+            << "______________________________________________"
+            << std::endl
+            << std::endl;
 
+  std::vector<int> dataset;
+  for( unsigned int i = 0; i < 15; ++i )
+  {
+    dataset.push_back( std::rand() % 100 );
+  }
+  
+  std::cout << "Sort():"
+            << std::endl;
+  
+  std::cout << "Before sorting, the randomly generated dataset is ";
+  PrintVectorHorizontal( dataset );
+  std::cout << std::endl;
+  
+  Sort( dataset );
+  
+  std::cout << "After sorting,  the randomly generated dataset is ";
+  PrintVectorHorizontal( dataset );
+  std::cout << std::endl;
+  
+  std::cout << std::endl;
   return 0;
 }
