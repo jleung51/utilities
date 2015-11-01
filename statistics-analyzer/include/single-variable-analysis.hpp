bdfc58534bd0ca2caf7d497cc5e73650fad1a015
jleung51 <jleung513@gmail.com>
2015-11-01 01:50:30 -0800

    StatAnalyzer: Moving IQR() to above LowerLimit(). Weird on Git due to same descriptions.

--- statistics-analyzer/include/single-variable-analysis.hpp
+++ statistics-analyzer/include/single-variable-analysis.hpp
@@ -78,6 +78,14 @@ T Maximum( const std::vector<T>& dataset );
 template <class T>
 T Quartile( const std::vector<T>& dataset, unsigned int quartile_num );
 
+// This function returns the IQR of a vector dataset.
+// The 1.5 IQR rule is used.
+// An exception is thrown if:
+//   The dataset is empty (length_error)
+//   The dataset is not sorted in ascending order (invalid_argument)
+template <class T>
+T InterQuartileRange( const std::vector<T>& dataset );
+
 // This function returns the lower limit of a vector dataset.
 // The 1.5 IQR rule is used.
 // An exception is thrown if:
@@ -93,14 +101,6 @@ unsigned int LowerLimit( const std::vector<T>& dataset );
 //   The dataset is not sorted in ascending order (invalid_argument)
 template <class T>
 unsigned int UpperLimit( const std::vector<T>& dataset );
-
-// This function returns the IQR of a vector dataset.
-// The 1.5 IQR rule is used.
-// An exception is thrown if:
-//   The dataset is empty (length_error)
-//   The dataset is not sorted in ascending order (invalid_argument)
-template <class T>
-T InterQuartileRange( const std::vector<T>& dataset );
 
 // This function returns the outliers of a vector dataset into a given vector.
 // The 1.5 IQR rule is used.
