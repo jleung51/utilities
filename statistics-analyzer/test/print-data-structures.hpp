b46f3eadf79366f49454958d7b2d78be6a75c204
jleung51 <jleung513@gmail.com>
2015-11-01 12:55:27 -0800

    StatAnalyzer: Adding PrintVectorVertical() function.

--- statistics-analyzer/test/print-data-structures.hpp
+++ statistics-analyzer/test/print-data-structures.hpp
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-10-30
+ * Last edited: 2015-11-01
  *
  * This C++ header file contains functions which can be used to print
  * the contents of an array or vector.
@@ -26,8 +26,12 @@ void PrintArrayHorizontal( T* arr, unsigned int len );
 template <class T>
 void PrintVectorHorizontal( std::vector<T> v );
 
+// This function prints the contents of a given vector in a vertical list.
+template <class T>
+void PrintVectorVertical( std::vector<T> v );
 
-// Function implementations::
+
+// Function implementations:
 
 // This function prints the contents of a given array in a horizontal list,
 // surrounded by square brackets [].
@@ -60,6 +64,19 @@ void PrintVectorHorizontal( std::vector<T> v )
   
   std::cout << "]";
 
+  return;
+}
+
+// This function prints the contents of a given vector in a vertical list.
+template <class T>
+void PrintVectorVertical( std::vector<T> v )
+{
+  for( unsigned int i = 0; i < v.size(); ++i )
+  {
+    std::cout << v[i]
+              << std::endl;
+  }
+  
   return;
 }
 
