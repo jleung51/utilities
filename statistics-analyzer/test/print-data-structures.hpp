ace90b844efb3dac0f17e4d2e05c6f1921a74441
jleung51 <jleung513@gmail.com>
2015-11-15 00:09:33 -0800

    StatAnalyzer: Changing printing functions to use iterators (plus Atom's formatting voodoo).

--- statistics-analyzer/test/print-data-structures.hpp
+++ statistics-analyzer/test/print-data-structures.hpp
@@ -1,10 +1,10 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-11-01
+ * Last edited: 2015-11-14
  *
- * This C++ header file contains functions which can be used to print
- * the contents of an array or vector.
+ * This C++ header file contains a function to print the contents of an
+ * iterable data structure.
  *
  */
 
@@ -13,74 +13,29 @@
 
 #include <iostream>
 #include <iterator>
-#include <vector>
 
 // Function prototypes:
 
-// This function prints the contents of a given array in a horizontal list,
-// surrounded by square brackets [].
-template <class T>
-void PrintArrayHorizontal( T* arr, unsigned int len );
-
-// This function prints the contents of a given vector in a horizontal list,
-// surrounded by square brackets [].
-template <class T>
-void PrintVectorHorizontal( std::vector<T> v );
-
-// This function prints the contents of a given vector in a vertical list.
-template <class T>
-void PrintVectorVertical( std::vector<T> v );
-
+// This function prints the contents of a given iterable data
+// structure in a horizontal list, surrounded by brackets [].
+template <class Iterator>
+void PrintHorizontal( const Iterator begin, const Iterator end );
 
 // Function implementations:
 
-// This function prints the contents of a given array in a horizontal list,
-// surrounded by square brackets [].
-template <class T>
-void PrintArrayHorizontal( T* arr, unsigned int len )
+// This function prints the contents of a given iterable data
+// structure in a horizontal list, surrounded by brackets [].
+template <class Iterator>
+void PrintHorizontal( const Iterator begin, const Iterator end )
 {
   std::cout << "[ ";
-  for( unsigned int i = 0; i < len; ++i )
+  unsigned int i = 0;
+  for( Iterator index = begin; index != end; std::advance(index, 1) )
   {
-    std::cout << arr[i] << " ";
+    std::cout << *index << " ";
+    ++i;
   }
   std::cout << "]";
-  
-  return;
-}
-
-// This function prints the contents of a given vector in a horizontal list,
-// surrounded by square brackets [].
-template <class T>
-void PrintVectorHorizontal( std::vector<T> v )
-{
-  std::cout << "[ ";
-  
-  for( typename std::vector<T>::iterator v_index = v.begin();
-       v_index != v.end();
-       std::advance( v_index, 1 ) )
-  {
-    std::cout << *v_index
-              << " ";
-  }
-  
-  std::cout << "]";
-
-  return;
-}
-
-// This function prints the contents of a given vector in a vertical list.
-template <class T>
-void PrintVectorVertical( std::vector<T> v )
-{
-  for( typename std::vector<T>::iterator v_index = v.begin();
-       v_index != v.end();
-       std::advance( v_index, 1 ) )
-  {
-    std::cout << *v_index
-              << std::endl;
-  }
-  
   return;
 }
 
--- statistics-analyzer/test/test.cpp
+++ statistics-analyzer/test/test.cpp
@@ -27,7 +27,7 @@ int main()
             << "______________________________________________"
             << std::endl
             << std::endl;
-  
+
   std::cout << "Sort():"
             << std::endl;
 
@@ -36,30 +36,30 @@ int main()
   {
     dataset_sort.push_back( rand() % 100 );
   }
-  
+
   std::cout << "Before sorting, the randomly generated dataset is ";
-  PrintVectorHorizontal( dataset_sort );
+  PrintHorizontal( dataset_sort.begin(), dataset_sort.end() );
   std::cout << std::endl;
-  
+
   Sort( dataset_sort );
-  
+
   std::cout << "After sorting,  the randomly generated dataset is ";
-  PrintVectorHorizontal( dataset_sort );
+  PrintHorizontal( dataset_sort.begin(), dataset_sort.end() );
   std::cout << std::endl
             << std::endl;
-  
-  
+
+
   std::cout << "Mean() and Mean_ToFloat():"
             << std::endl;
-  
+
   std::vector<int> dataset_mean;
   for( unsigned int i = 0; i < 3; ++i )
   {
     dataset_mean.push_back( rand() % 5 );
   }
-  
+
   std::cout << "The mean of the randomly generated integer dataset ";
-  PrintVectorHorizontal( dataset_mean );
+  PrintHorizontal( dataset_mean.begin(), dataset_mean.end() );
   std::cout << " is "
             << Mean( dataset_mean )
             << " as an integer, and "
@@ -71,7 +71,7 @@ int main()
 
   std::cout << "Mode():"
             << std::endl;
-  
+
   std::vector<int> dataset_mode;
   dataset_mode.push_back( 1 );
   dataset_mode.push_back( 1 );
@@ -84,64 +84,64 @@ int main()
   dataset_mode.push_back( 7 );
   dataset_mode.push_back( 7 );
   dataset_mode.push_back( 7 );
-  
+
   std::vector<int> dataset_mode_modes;
   Mode( dataset_mode, dataset_mode_modes );
-  
+
   std::cout << "The modes of the dataset ";
-  PrintVectorHorizontal( dataset_mode );
+  PrintHorizontal( dataset_mode.begin(), dataset_mode.end() );
   std::cout << " are ";
-  PrintVectorHorizontal( dataset_mode_modes );
+  PrintHorizontal( dataset_mode_modes.begin(), dataset_mode_modes.end() );
   std::cout << "."
             << std::endl
             << std::endl;
-            
-  
+
+
   std::cout << "Median():"
             << std::endl;
-  
+
   std::vector<int> dataset_median_odd;
   for( unsigned int i = 0; i < 5; ++i )
   {
     dataset_median_odd.push_back( i );
   }
-  
+
   std::vector<int> dataset_median_even;
   for( unsigned int i = 0; i < 4; ++i )
   {
     dataset_median_even.push_back( i*2 );
   }
-  
+
   std::cout << "The median of the dataset ";
-  PrintVectorHorizontal( dataset_median_odd );
+  PrintHorizontal( dataset_median_odd.begin(), dataset_median_odd.end() );
   std::cout << " is "
             << Median( dataset_median_odd )
             << "."
             << std::endl;
-  
+
   std::cout << "The median of the dataset ";
-  PrintVectorHorizontal( dataset_median_even );
+  PrintHorizontal( dataset_median_even.begin(), dataset_median_even.end() );
   std::cout << " is "
             << Median( dataset_median_even )
             << "."
             << std::endl
             << std::endl;
-  
-  
+
+
   std::cout << "Quartile() and Quartile_ToFloat():"
             << std::endl;
-  
+
   std::vector<int> dataset_quartile_7;
   for( unsigned int i = 0 ; i < 7; ++i )
   {
     dataset_quartile_7.push_back( i );
   }
-  
+
   std::cout << "From the dataset ";
-  PrintVectorHorizontal( dataset_quartile_7 );
+  PrintHorizontal( dataset_quartile_7.begin(), dataset_quartile_7.end() );
   std::cout << ":"
             << std::endl;
-            
+
   std::cout << "  Q1 = "
             << Quartile( dataset_quartile_7, 1 )
             << std::endl;
@@ -152,18 +152,18 @@ int main()
             << Quartile( dataset_quartile_7, 3 )
             << std::endl
             << std::endl;
-            
+
   std::vector<int> dataset_quartile_10;
   for( unsigned int i = 0 ; i < 10; ++i )
   {
     dataset_quartile_10.push_back( i );
   }
-  
+
   std::cout << "From the dataset ";
-  PrintVectorHorizontal( dataset_quartile_10 );
+  PrintHorizontal( dataset_quartile_10.begin(), dataset_quartile_10.end() );
   std::cout << ":"
             << std::endl;
-            
+
   std::cout << "  Q1 = "
             << Quartile_ToFloat( dataset_quartile_10, 1 )
             << std::endl;
@@ -174,6 +174,6 @@ int main()
             << Quartile_ToFloat( dataset_quartile_10, 3 )
             << std::endl
             << std::endl;
-  
+
   return 0;
 }
